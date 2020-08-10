2fdabaf4e84cfffff78e17c43ee1772f19d37ad6
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-08-10 19:23:59 +0100

    prototype in progress

--- Prototype/Player.cs
+++ Prototype/Player.cs
@@ -28,10 +28,15 @@ namespace Prototype
         FullBack
     }
 
+    public enum GameSide
+    {
+        LeftSide,
+        RightSide
+    }
+
     public class Player
     {
         public Position FieldPosition { get; set; }
-
         public PlayerPosition JerseyPosition { get; set; }
 
         public Player()
@@ -39,10 +44,39 @@ namespace Prototype
         }
     }
 
+    public class MatchPlayer
+    {
+        public Player PlayerEntry { get; private set; }
+        public GameSide Side { get; private set; }
+
+        public MatchPlayer(Player player, GameSide side)
+        {
+            PlayerEntry = player;
+            Side = side;
+        }
+    }
+
+    public class Game
+    {
+        MatchPlayer[] Players;
+
+        // private
+        /// List of players
+    }
+
+    public class RugbyTeam
+    {
+        public Player[] Team;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Game game = new Game();
+
+            Player p1 = new Player();
+            Player p2 = new Player();
         }
     }
 }
