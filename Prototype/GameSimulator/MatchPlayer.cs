75ab6f972a92c72b869cf7912ad76e304dc16858
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-08-11 19:12:23 +0100

    added class

--- Prototype/GameSimulator/MatchPlayer.cs
+++ Prototype/GameSimulator/MatchPlayer.cs
@@ -2,5 +2,19 @@
 {
     namespace GameSimulator
     {
+        public class MatchPlayer
+        {
+            public Player PlayerEntry { get; private set; }
+            public GameSide Side { get; private set; }
+
+            public Position FieldPosition { get; set; }
+
+            public MatchPlayer(Player player, GameSide side, Position position)
+            {
+                PlayerEntry = player;
+                Side = side;
+                FieldPosition = position;
+            }
+        }
     }
 }
