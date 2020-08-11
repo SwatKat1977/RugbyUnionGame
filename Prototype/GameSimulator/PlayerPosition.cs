5e89797e11397211afd5d567e88e64a440eb513f
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-08-11 18:40:53 +0100

    changes

---
+++ Prototype/GameSimulator/PlayerPosition.cs
@@ -0,0 +1,31 @@
+﻿namespace Prototype
+{
+    namespace GameSimulator
+    {
+        public enum PlayerPosition
+        {
+            /// <summary>
+            /// The loose-head and tight-head props make up what is known as the
+            /// front row, which refers to their position in the scrum.
+            /// </summary>
+            Prop,
+
+            /// <summary>
+            /// Lining up in the scrum between the two props, the hooker is one of
+            /// the forwards’ key decision-makers. He or she will coordinate the
+            /// timing at the scrum, and is also responsible for winning possession
+            /// in the scrummage by hooking the ball back through the props' legs.
+            /// </summary>
+            Hooker,
+
+            LockSecondRow,
+            FlankerWingForward,
+            Number8,
+            ScrumHalf,
+            FlyHalf,
+            Wing,
+            Centre,
+            FullBack
+        }
+    }
+}
