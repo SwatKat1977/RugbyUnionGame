33f1d727b3e095c10a321aab7942bcadc0be9b69
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-09-22 12:05:10 +0100

    work on game engine

---
+++ Prototype/GameSimulator/PlayerInformation.cs
@@ -0,0 +1,19 @@
+﻿namespace Prototype.GameSimulator
+{
+    public class PlayerInformation
+    {
+        public string Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string TeamId { get; private set; }
+
+
+        public PlayerInformation(string id, string name, string team = null)
+        {
+            Id = id;
+            Name = name;
+            TeamId = team;
+        }
+    }
+}
---
+++ Prototype/GameSimulator/Team.cs
@@ -0,0 +1,15 @@
+﻿namespace Prototype.GameSimulator
+{
+    public class Team
+    {
+        public string Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public Team(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
